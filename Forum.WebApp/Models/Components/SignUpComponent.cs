f4329772f17ba489acc4ab15a463d69832c11918
Peklerfix <peklerfix@gmail.com>
2022-05-30 14:05:05 +0200

    All vies for adding entities has been changed for view components

---
+++ Forum.WebApp/Models/Components/SignUpComponent.cs
@@ -0,0 +1,13 @@
+﻿using Forum.WebApp.Models.Account;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Forum.WebApp.Models.Components
+{
+    public class SignUpViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(RegisterFM model)
+        {
+            return View();
+        }
+    }
+}
