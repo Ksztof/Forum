1b7deb0083e50d2b87345cbaf41866bbb2b2fc37
Peklerfix <peklerfix@gmail.com>
2022-05-30 13:34:50 +0200

    view component  for question add has been added

---
+++ Forum.WebApp/Models/Components/QuestionAddComponent.cs
@@ -0,0 +1,13 @@
+﻿using Forum.Core.Models.Question;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Forum.WebApp.Models.Components
+{
+    public class QuestionAddViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(QuestionAddFM model)
+        {
+            return View();
+        }
+    }
+}
